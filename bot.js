285a2cfce2564170fd4277a0200ddd716bfeef84
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-05-28 14:52:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,8 +26,8 @@ client.user.setStatus("dnd")
 
 client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefix = "Z";
-    if(message.content.startsWith('Zbc')) {
+    var prefix = "$";
+    if(message.content.startsWith('$bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
