38668b1b7d050ee6cd4254c576593b4c5d989843
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-05-28 14:55:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,20 +26,20 @@ client.user.setStatus("dnd")
 
 client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefix = "$";
     if(message.content.startsWith('$bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "Z Bot";
+    let copy = "Dragon";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
     .then(() => msg.react('❌'))
     .then(() =>msg.react('✅'))
+    
     let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
     let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
- let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
     let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
     reaction1.on("collect", r => {
     message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
@@ -51,7 +51,6 @@ client.on('message', message => {
        .addField('Server', message.guild.name)
        .addField('Sender', message.author.username)
        .addField('Message', args)
-       .setImage("هنا حط رابط الصوره اللي بتظهر")
        .setThumbnail(message.author.avatarURL)
        .setFooter(copy, client.user.avatarURL);
     m.send({ embed: bc })
@@ -64,7 +63,7 @@ client.on('message', message => {
     })
     })
     }
-    });        
+    });    
         
 
 
