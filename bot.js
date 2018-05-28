83efb627dd7b06bafc039ca735f286ae4f9d344f
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-05-28 10:40:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,16 @@ client.on('message', message => {
 });
 
 
-
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`🌈رمضان كريم🌙`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
 
 
 // THIS  MUST  BE  THIS  WAY
