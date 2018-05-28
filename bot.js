665564a57edcbb1c7c063ee5dc3e6a4782178682
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-05-28 15:00:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', message => {
 
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "bc3")) {
+            if (message.content.startsWith(prefix + "$bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
