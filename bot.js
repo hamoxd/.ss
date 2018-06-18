93e647566256e97345eb5dfb523c6527fdff5733
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-06-18 03:58:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,13 +27,24 @@ client.on("message", message => {
       .setColor("#ffff00")
       .setThumbnail(message.author.avatarURL)
       .setDescription(`
+***WELCOME TO ZOMBIE***
 
-
+https://discord.gg/2UJV6Hp
 
 message.author.sendEmbed(embed)
 
 }
 }); 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Z_4Ever`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
